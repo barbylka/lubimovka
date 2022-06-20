320921df108cfc9b78ce480f3ef98aa9272aca43
Artem Schirov <78075439+artemschirov@users.noreply.github.com>
2022-06-20 22:56:20 +0300

    add: hamburger menu button position and hide scrolling

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -1,17 +1,31 @@
+const body = document.querySelector(".body");
 const hamburgerButton = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".topnav__menu");
-const navLink = document.querySelectorAll(".topnav__item");
+const navLink = document.querySelectorAll(".topnav__item-link");
+let navMenuOpened = false;
+
+function stopPageScroll() {
+  navMenuOpened = !navMenuOpened;
+  if (navMenuOpened) {
+    body.style.overflowY = "hidden";
+    navMenuOpened = true;
+  } else {
+    navMenuOpened = false;
+    body.style.overflowY = "scroll";
+  }
+}
 
 function toggleNavMenu() {
   hamburgerButton.classList.toggle("opened");
   navMenu.classList.toggle("opened");
+  stopPageScroll(navMenuOpened);
 }
 
 function closeMenu() {
-  console.log('here: ', );
   hamburgerButton.classList.remove("opened");
   navMenu.classList.remove("opened");
+  stopPageScroll(navMenuOpened);
 }
 
 hamburgerButton.addEventListener("click", toggleNavMenu);
-navLink.forEach(link => link.addEventListener("click", closeMenu));
+navLink.forEach((link) => link.addEventListener("click", closeMenu));
